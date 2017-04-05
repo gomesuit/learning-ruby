c5797c5c874ce476cb2c4046af4c43425b24cbf3
gomesuit <gomesuit@gmail.com>
2017-04-06 04:13:55 +0900

    サンプルを追加

--- gem/mechanize/README.md
+++ gem/mechanize/README.md
@@ -2,3 +2,9 @@
 # 参考
 # https://www.xmisao.com/2013/10/05/ruby-www-mechanize.html
 
+
+```
+bundle install
+bundle exec ruby sample.rb
+```
+
--- gem/mechanize/sample.rb
+++ gem/mechanize/sample.rb
@@ -1,3 +1,29 @@
 require 'mechanize'
+require 'pry'
 
+agent = Mechanize.new
+page = agent.get('https://www.google.co.jp/')
+
+# body
+p page.body
+
+# forms
+p page.forms
+
+# links
+p page.links
+
+# cookie
+p agent.cookies
+
+# submit
+form = page.forms[0]
+form.q = "ゼルダ"
+result = agent.submit(form)
+
+# click
+result.links[0].click
+
+# 検索結果の一つ目をクリック
+result.links_with(href: /url/)[0].click
 
