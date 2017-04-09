0ea45d8384266b4ca4848deba42deea44cb1a468
gomesuit <gomesuit@gmail.com>
2017-04-09 23:22:36 +0900

    ダウンロードしたZIPを保存せずにデータを拾う

--- open-uri/sample2.rb
+++ open-uri/sample2.rb
@@ -1,14 +1,20 @@
 require 'csv'
 require 'pry'
 require 'open-uri'
+require "zip"
+require 'kconv'
+
 @path = 'http://www.post.japanpost.jp/zipcode/dl/oogaki/zip/13tokyo.zip'
 
-fileName = File.basename(@path)
-
-open(fileName, 'wb') do |output|
-  open(@path) do |data|
-    output.write(data.read)
+open(URI.escape(@path)) do |file|
+  Zip::File.open_buffer(file.read) do |zf|
+    zf.each do |entry|
+      #p entry.name # ファイル名
+      #p entry.get_input_stream.read # ファイルの中身
+      CSV.parse(entry.get_input_stream.read).each do |row|
+        p row.map(&:toutf8)
+      end
+    end
   end
 end
-
 
