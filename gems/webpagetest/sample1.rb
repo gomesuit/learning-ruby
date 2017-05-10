f9a7b6375b8658b54067b13d38598d488a4e0df4
gomesuit <takashi.inoue@theport.jp>
2017-05-11 03:22:47 +0900

    save

--- gems/webpagetest/sample1.rb
+++ gems/webpagetest/sample1.rb
@@ -6,8 +6,16 @@ raise if ENV['API_KEY'].nil?
 
 wpt = Webpagetest.new(k: ENV['API_KEY'])
 
+locations = wpt.locations
+puts locations.keys
+
 response = wpt.run_test do |params|
   params.url = 'http://webpagetest.org'
+  params.location = 'ec2-ap-northeast-1'
+  params.video = 1
+  params.mobile = 1
+  params.fvonly = 1
+  params.runs = 3
 end
 
 loop do
