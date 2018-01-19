039df3687abdf6dba820843b0d99ad802aa777af
gomesuit <gomesuit@gmail.com>
2018-01-20 03:27:48 +0900

    save

--- gems/mechanize/amazon3.rb
+++ gems/mechanize/amazon3.rb
@@ -1,17 +1,17 @@
 require 'mechanize'
 require 'pry'
-
+require 'retryable'
 
 asins = []
 Mechanize.start do |m|
   m.user_agent_alias = 'Mac Safari'
-  retry_on_error times: 20 do
+  Retryable.retryable(tries: 10, sleep: 3) do
     m.get('https://www.amazon.co.jp/s/?rh=n%3A2250738051%2Cn%3A%212250739051%2Cn%3A2275256051%2Cn%3A2293143051%2Cp_n_date%3A2275273051')
   end
   loop do
     m.page.parser.css('li.s-result-item.celwidget').each do |node|
       # 画像がないものはスキップ
-      next if node.css('.s-access-image.cfMarker').blank?
+      next if node.css('.s-access-image.cfMarker').nil?
       next if node.css('.s-access-image.cfMarker').attribute('src').value.include? 'no-img'
 
       asin = node.attributes['data-asin'].value
@@ -20,9 +20,10 @@ Mechanize.start do |m|
     end
     link = m.page.link_with(id: 'pagnNextLink')
     break if link.nil?
-    sleep 5
-    retry_on_error times: 20 do
+    Retryable.retryable(tries: 10, sleep: 5) do
       link.click
     end
   end
 end
+
+pp asins
