b7fc1ee390ce33260e3bac271752bb5f16a41a99
gomesuit <gomesuit@gmail.com>
2018-01-20 03:07:41 +0900

    add sample

---
+++ gems/mechanize/amazon2.rb
@@ -0,0 +1,15 @@
+require 'mechanize'
+require 'pry'
+
+m = Mechanize.new
+m.user_agent_alias = 'Mac Safari'
+
+m.get('https://www.amazon.co.jp/dp/B078K85TZQ')
+
+b = m.page.parser.css('.swatchElement a.a-button-text')[1].attributes['href'].value
+m.get b
+
+# ISBN-10
+pp m.page.parser.css('#detail_bullets_id .content ul li')[3].children[1].text
+# ISBN-13
+pp m.page.parser.css('#detail_bullets_id .content ul li')[4].children[1].text
