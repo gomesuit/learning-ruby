9cf986b7e847580f04d5d16981bb8c33bf4a221b
gomesuit <gomesuit@gmail.com>
2018-01-20 03:13:19 +0900

    save

--- gems/mechanize/amazon2.rb
+++ gems/mechanize/amazon2.rb
@@ -10,6 +10,6 @@ b = m.page.parser.css('.swatchElement a.a-button-text')[1].attributes['href'].va
 m.get b
 
 # ISBN-10
-pp m.page.parser.css('#detail_bullets_id .content ul li')[3].children[1].text
+pp m.page.parser.css('#detail_bullets_id .content ul li')[3].children[1].text.strip
 # ISBN-13
-pp m.page.parser.css('#detail_bullets_id .content ul li')[4].children[1].text
+pp m.page.parser.css('#detail_bullets_id .content ul li')[4].children[1].text.strip.gsub('-', '')
