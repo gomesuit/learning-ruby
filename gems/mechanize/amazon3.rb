5d09968980fb756c69c27208dbea84141a45005e
gomesuit <gomesuit@gmail.com>
2018-01-21 04:14:49 +0900

    save

--- gems/mechanize/amazon3.rb
+++ gems/mechanize/amazon3.rb
@@ -8,22 +8,14 @@ Mechanize.start do |m|
   Retryable.retryable(tries: 10, sleep: 3) do
     m.get('https://www.amazon.co.jp/s/?rh=n%3A2250738051%2Cn%3A%212250739051%2Cn%3A2275256051%2Cn%3A2293143051%2Cp_n_date%3A2275273051')
   end
-  loop do
-    m.page.parser.css('li.s-result-item.celwidget').each do |node|
-      # 画像がないものはスキップ
-      next if node.css('.s-access-image.cfMarker').nil?
-      next if node.css('.s-access-image.cfMarker').attribute('src').value.include? 'no-img'
+  m.page.parser.css('li.s-result-item.celwidget').each do |node|
+    # 画像がないものはスキップ
+    next if node.css('.s-access-image.cfMarker').nil?
+    next if node.css('.s-access-image.cfMarker').attribute('src').value.include? 'no-img'
 
-      asin = node.attributes['data-asin'].value
-      pp asin
-      asins.push asin
-    end
-    link = m.page.link_with(id: 'pagnNextLink')
-    break if link.nil?
-    sleep 1
-    Retryable.retryable(tries: 10, sleep: 5) do
-      link.click
-    end
+    asin = node.attributes['data-asin'].value
+    pp asin
+    asins.push asin
   end
 end
 
