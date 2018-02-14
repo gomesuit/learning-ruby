41f9532b79a3795f2965116277f88b1ac219cb1e
gomesuit <gomesuit@gmail.com>
2018-02-14 14:53:58 +0900

    save

---
+++ gems/redcarpet/original_html.rb
@@ -0,0 +1,6 @@
+class OriginalHTML < Redcarpet::Render::HTML
+  def header(text, level)
+    level += 1
+    "<h#{level}>#{text}</h#{level}>"
+  end
+end
--- gems/redcarpet/sample.rb
+++ gems/redcarpet/sample.rb
@@ -1,4 +1,8 @@
 require 'redcarpet'
+require './original_html.rb'
 
 markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
 pp markdown.render("This is *bongos*, indeed.")
+
+renderer = Redcarpet::Markdown.new(OriginalHTML.new, autolink: true, tables: true)
+pp renderer.render('This is *bongos*, indeed.')
