e564466feff6894d67f5aeba1a6c5754b2a89212
gomesuit <gomesuit@gmail.com>
2018-02-27 20:12:43 +0900

    save

---
+++ gems/google-api-client/sample3.rb
@@ -0,0 +1,24 @@
+require 'googleauth/stores/redis_token_store'
+require 'google/apis/analyticsreporting_v4'
+
+view_id = 'XXXXXXXX'
+scope = ['https://www.googleapis.com/auth/analytics.readonly']
+analytics =Google::Apis::AnalyticsreportingV4
+
+client = analytics::AnalyticsReportingService.new
+client.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
+  json_key_io: File.open('service-account-key.json'),
+  scope: scope
+)
+
+date_range = analytics::DateRange.new(start_date: '7DaysAgo', end_date: 'today')
+metric = analytics::Metric.new(expression: 'ga:sessions', alias: 'sessions')
+dimension = analytics::Dimension.new(name: 'ga:browser')
+request = analytics::GetReportsRequest.new(
+  report_requests: [analytics::ReportRequest.new(
+    view_id: view_id, metrics: [metric], dimensions: [dimension], date_ranges: [date_range]
+  )]
+)
+response = client.batch_get_reports(request)
+
+pp response.reports
