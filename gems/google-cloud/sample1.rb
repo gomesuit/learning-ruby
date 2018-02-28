071df817edceedece45e3adcb29f6a90c6aa6157
gomesuit <gomesuit@gmail.com>
2018-03-01 03:42:21 +0900

    save

---
+++ gems/google-cloud/sample1.rb
@@ -0,0 +1,16 @@
+# Imports the Google Cloud client library
+require "google/cloud/bigquery"
+
+# Instantiates a client
+bigquery = Google::Cloud::Bigquery.new(
+  project: 'xxxxxxxxxxx',
+  keyfile: './service-account-key.json'
+)
+
+# The name for the new dataset
+dataset_name = "my_new_dataset"
+
+# Creates the new dataset
+dataset = bigquery.create_dataset dataset_name
+
+puts "Dataset #{dataset.dataset_id} created."
