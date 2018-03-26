1dc3ba9d2932b8599619722fb1b7f46faaebccf5
gomesuit <gomesuit@gmail.com>
2018-03-27 03:12:44 +0900

    save

--- gem-package/export_param_store/lib/export_param_store.rb
+++ gem-package/export_param_store/lib/export_param_store.rb
@@ -1,5 +1,43 @@
 require "export_param_store/version"
+require "aws-sdk-ssm"
+require 'erb'
 
 module ExportParamStore
   # Your code goes here...
+  def self.start(path)
+    @path = path
+
+    client = Aws::SSM::Client.new
+    envs = client.get_parameters_by_path(
+      {
+        path: @path,
+        with_decryption: true
+      }
+    )
+
+    @params = []
+    token = nil
+
+    loop do
+      parameters = client.get_parameters_by_path(
+        {
+          path: @path,
+          with_decryption: true,
+          next_token: token
+        }
+      )
+      @params.concat(parameters[0])
+      token = parameters[1]
+      break if token.nil?
+    end
+
+    contents = <<-ERB
+<% @params.each do |param| %>
+export <%= param.name.delete(@path) %>=<%= param.value %>
+<% end %>
+    ERB
+
+    puts ERB.new(contents, nil, 2).result(binding)
+  end
 end
+
