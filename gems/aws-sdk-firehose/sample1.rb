57e8dabc95fe13eaa1b0f9ee6b5dc3c502bb6b47
gomesuit <gomesuit>
2018-05-06 21:00:37 +0900

    save

--- gems/aws-sdk-firehose/sample1.rb
+++ gems/aws-sdk-firehose/sample1.rb
@@ -10,6 +10,15 @@ resp = client.put_record({
   },
 })
 
+resp = client.put_record_batch({
+  delivery_stream_name: "firehose-test", # required
+  records: [ # required
+    {
+      data: "data", # required
+    },
+  ],
+})
+
 binding.pry
 
 pp 'finish'
