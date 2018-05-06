2ae79d5b147067827a602d85bedbe72edd0498d2
gomesuit <gomesuit>
2018-05-06 22:31:03 +0900

    save

--- gems/aws-sdk-firehose/sample1.rb
+++ gems/aws-sdk-firehose/sample1.rb
@@ -3,10 +3,12 @@ require 'aws-sdk-firehose'
 
 client = Aws::Firehose::Client.new
 
+data = '{"player":{"username":"user1","test":[1,2,3],"characteristics":{"race":"Human","class":"Warlock","subclass":"Dawnblade","power":300,"playercountry":"USA"},"arsenal":{"kinetic":{"name":"Sweet Business","type":"Auto Rifle","power":300,"element":"Kinetic"},"energy":{"name":"MIDA Mini-Tool","type":"Submachine Gun","power":300,"element":"Solar"},"power":{"name":"Play of the Game","type":"Grenade Launcher","power":300,"element":"Arc"}},"armor":{"head":"Eye of Another World","arms":"Philomath Gloves","chest":"Philomath Robes","leg":"Philomath Boots","classitem":"Philomath Bond"},"location":{"map":"Titan","waypoint":"The Rig"}}}'
+
 resp = client.put_record({
   delivery_stream_name: "firehose-test", # required
   record: { # required
-    data: "data", # required
+    data: data, # required
   },
 })
 
@@ -14,7 +16,7 @@ resp = client.put_record_batch({
   delivery_stream_name: "firehose-test", # required
   records: [ # required
     {
-      data: "data", # required
+      data: data, # required
     },
   ],
 })
