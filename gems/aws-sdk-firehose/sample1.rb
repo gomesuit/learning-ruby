4d1761efe4f4b4b88925a4388f2ddd4df9f6d180
gomesuit <gomesuit>
2018-05-06 20:58:20 +0900

    save

--- gems/aws-sdk-firehose/sample1.rb
+++ gems/aws-sdk-firehose/sample1.rb
@@ -3,6 +3,13 @@ require 'aws-sdk-firehose'
 
 client = Aws::Firehose::Client.new
 
+resp = client.put_record({
+  delivery_stream_name: "firehose-test", # required
+  record: { # required
+    data: "data", # required
+  },
+})
+
 binding.pry
 
 pp 'finish'
