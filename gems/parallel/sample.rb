3e17b60a42e1655054c384ba175b9425cc69bbc5
gomesuit <gomesuit>
2018-05-17 12:22:41 +0900

    save

--- gems/parallel/sample.rb
+++ gems/parallel/sample.rb
@@ -1,6 +1,7 @@
+require 'parallel'
 
 def func(i)
-  puts "#### Function in Multi Thread : index=#{i}"
+  puts "#### Function in Multi Thread : index=#{i}\n"
 end
 
 Parallel.each(1..1000, in_threads: 8) { |i|
