7b42859967e2eb87eab047a773bca54c13354a19
gomesuit <9404411+gomesuit@users.noreply.github.com>
2020-11-18 22:21:37 +0900

    save

--- gems/browser/main.rb
+++ gems/browser/main.rb
@@ -3,3 +3,5 @@ require "browser"
 browser = Browser.new('Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1')
 
 pp browser.device.mobile?
+pp browser.platform.ios?
+pp browser.safari?
